40bc99ba0c03a540f8c47c15322a4b7f54f26084
Michal Bocek <michal.bocek@gmail.com>
2019-05-12 18:44:16 +0200

    feat(application): initial commit

---
+++ food/src/main/groovy/org/eating/food/HelloController.groovy
@@ -0,0 +1,23 @@
+package org.eating.food
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+@Controller("/hello")
+class HelloController {
+
+    @Get(value = "/", produces = MediaType.APPLICATION_JSON)
+    HttpResponse<Response> index() {
+        return HttpResponse.ok().body(new Response("Hello"))
+    }
+
+    class Response {
+        String message
+
+        Response(String message) {
+            this.message = message
+        }
+    }
+}
